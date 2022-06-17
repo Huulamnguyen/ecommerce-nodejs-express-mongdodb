cbe26e00bb95e1d0757edd49a77bc650529041c7
huulamnguyen <liamnguyen.swe@gmail.com>
2022-06-17 13:24:43 -0400

    Completed full CRUB Admin product

---
+++ models/product.js
@@ -0,0 +1,26 @@
+const { MongoParseError } = require('mongodb');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// todo: design productSchema
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Product", productSchema);
