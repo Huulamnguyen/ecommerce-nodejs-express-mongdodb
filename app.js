12fb82dc3a6ccc26085f23b97f2fe19c0abc4e91
huulamnguyen <liamnguyen.swe@gmail.com>
2022-06-19 16:11:16 -0400

    Cookie And Session

--- app.js
+++ app.js
@@ -4,33 +4,54 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+// Initialize session from express-session
+const session = require('express-session');
+// Set up MongoDBStore to store session
+const MongoDBStore = require('connect-mongodb-session')(session);
+
 require('dotenv').config();
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URI,
+  collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'my secret', 
+  resave: false,
+  saveUninitialized: false,
+  store: store,
+}));
 
 app.use((req, res, next) => {
-  User.findById('62ad3d586081483d4a86fbad')
+  if(!req.session.user){
+    return next();
+  };
+  
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
     })
     .catch(err => console.log(err));
-});
+})
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes); //leading path /admin
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
